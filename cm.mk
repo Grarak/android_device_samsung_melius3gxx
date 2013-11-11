a8f0e2b406681872e2c2c97e7dddaf567e50ce30
Grarak <williye97@gmail.com>
2013-11-11 15:44:55 +0100

    Bring up cm-11.0 add overlays
    Signed-off-by: Grarak <williye97@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/melius3gxx/full_melius3gxx.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
